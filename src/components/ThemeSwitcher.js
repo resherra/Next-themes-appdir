b62b9c48204ceb0dda3ac697f28dbfc998c91a55
lazych <redouancherrat@gmail.com>
2023-04-25 01:22:04 +0000

    ready

---
+++ src/components/ThemeSwitcher.js
@@ -0,0 +1,20 @@
+"use client"
+
+import { useTheme } from "next-themes"
+
+export default function ThemeSwitcher() {
+  const { resolvedTheme, setTheme } = useTheme()
+
+  const handleSwitch = () => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark")
+  }
+
+  return (
+    <div>
+      <button onClick={handleSwitch} className={`bg-black text-pink-200 dark:bg-pink-200 dark:text-black text-sm rounded-md p-1 mb-4`}>
+        Switch theme
+      </button>
+      <div>the theme now is: {resolvedTheme === "dark" ? "/dark" : "/light"}</div>
+    </div>
+  )
+}
